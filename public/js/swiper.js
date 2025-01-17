92371651644c59e59d74e83e54b5b3215b6f279e
EriyaDev <eriyagamedev@gmail.com>
2025-01-17 09:06:18 +0800

    v1.0 No Anim

--- public/js/swiper.js
+++ public/js/swiper.js
@@ -9,8 +9,8 @@ var swiper = new Swiper(".mySwiper", {
     },
   });
 
-  var swiper2 = new Swiper(".swiperReview", {
-    slidesPerView: "auto",
+var trainerSwiper = new Swiper(".trainerSwiper", {
+    slidesPerView: 1,
     spaceBetween: 30,
     pagination: {
       el: ".swiper-pagination",
@@ -18,6 +18,50 @@ var swiper = new Swiper(".mySwiper", {
       bulletActiveClass: "my-custom-bullet-active",
       bulletClass: "my-custom-bullet",
     },
+  });
+
+  var swiper2 = new Swiper(".swiperPricing2", {
+    slidesPerView: 1.5,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var swiperReview = new Swiper(".swiperReview", {
+    slidesPerView: 1.5,
+    spaceBetween: 30,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      bulletActiveClass: "swiper-pagination-bullet-active",
+      bulletClass: "swiper-pagination-bullet",
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1.5,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 70,
+      },
+    },
+  });
+
+  var swiperPricing = new Swiper(".swiperPricing", {
+    effect: "cards",
+    grabCursor: true,
   });
 
   setInterval(() => {
